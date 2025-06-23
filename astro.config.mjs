e7e642c3a229f179fcaa68c2b0261e0e98e2c541
Bartosz Białecki <bartosz.bialecki@gmail.com>
2025-06-23 20:42:02 +0200

    Configure eslint, prettier, husky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [react()],
+  server: { port: 3000 },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
